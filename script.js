a85099730d3bf91b0561d433680dc84688901269
nathgarr <89517296+nathgarr@users.noreply.github.com>
2023-05-04 07:38:55 +0000

    player base stat

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const baseMonsterRegene = 0.5;
 
 const difficulty = document.querySelectorAll(".difficulty")
 
+const playerBaseHp = 15;
+const playerBaseHpRegene = 0.2;
+const playerBaseArmor = 0.4;
+const playerBaseDmg = 2.5;
+const playerBaseAtackSpeed = 0.51;
+const playerBaseRespawnTime = 0.995;
+const playerBaseDodgeRate = 0.1 + "%";
+const playerBaseCritRate = 0.1 + "%";
+const playerBaseCritDmg = 200 + "%";
